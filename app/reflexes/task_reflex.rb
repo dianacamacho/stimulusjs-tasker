b523cb26a40d5ad1d0f7c2d56838d8b0ac894a1e
diana camacho <dcam1901@gmail.com>
2021-03-28 03:13:01 -0500

    refactor to shared toggle reflex and tas partial

--- app/reflexes/task_reflex.rb
+++ app/reflexes/task_reflex.rb
@@ -1,8 +1,8 @@
 class TaskReflex < StimulusReflex::Reflex
-  def complete
+  def toggle
     # reflex receives html element from data-reflex
     # you have access to element's properties like dataset attrs
     # set in html, eg: data-id="10" gives element.dataset.id => "10"
-    Task.find(element.dataset.id).update(completed_at: Time.current)
+    Task.find(element.dataset.id).update(completed_at: element.checked ? Time.current : nil)
   end
 end
